2abe184347a718270373cb590867515177b1f175
AAKASH HANDA <AAKASH HANDA>
2017-09-14 19:06:45 +0530

    a

---
+++ server.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var formidable = require('formidable');
+var util = require('util');
+
+var server = http.createServer(function (req,res){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept');
+
+    if(req.method.toLowerCase() == 'post'){
+        processForm(req, res);
+        return;
+    }
+
+    res.end();
+});
+
+function processForm(req, res){
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields){
+        res.writeHead(200, {
+            'content-type':'text/plain'
+        });
+
+        res.end(util.inspect({
+            fields: fields
+        }));
+    });
+}
+
+var port = 3100;
+server.listen(port);
+console.log("server listen port "+3100)
